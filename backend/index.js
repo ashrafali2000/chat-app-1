d14fea1830bde16010cdaaf8246f8a0e35e4e783
ashrafali2000 <ali4282271@gmail.com>
2024-05-29 10:38:22 +0500

    add front url

--- backend/index.js
+++ backend/index.js
@@ -53,7 +53,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-1-7p6b.vercel.app/",
     credentials: true,
   },
 });
